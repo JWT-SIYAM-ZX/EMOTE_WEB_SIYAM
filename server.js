20fcf89c1319479f5c03809e9e0a9a4e204dae86
JWT-SIYAM-ZX <sppxxpp@gmail.com>
2025-12-30 22:18:51 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,22 +1,7 @@
 
-const express = require("express");
-const path = require("path");
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.get("/panel.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "panel.html"));
-});
-
-app.get("/admin.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "admin.html"));
-});
-
-app.listen(PORT, () => console.log("Server running"));
+const express=require("express");
+const path=require("path");
+const app=express();
+app.use(express.static(path.join(__dirname,"public")));
+app.get("/",(r,s)=>s.sendFile(path.join(__dirname,"public","index.html")));
+app.listen(process.env.PORT||3000);
